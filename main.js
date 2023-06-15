18e0eb8a1a67014c14329e09568ee8f34441a047
Nullify <46113492+NullifyDev@users.noreply.github.com>
2023-06-15 11:59:02 +0100

    Update main.js
    [main.js]: removed unnecessary lines. Made Code to be readable

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.clear()
-let fs = require("fs")
 const lexer = require("./src/lexer.js")
-const parser = require("./src/parser.js")
-const rawConf = JSON.parse(fs.readFileSync("./settings.json", "utf8"))
 
-parser.parse(lexer.lex(process.argv.slice(2)))
+parser.parse(
+  require("./src/lexer.js").lex(
+    process.argv.slice(2)
+  )
+)
