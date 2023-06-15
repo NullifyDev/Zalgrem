69fba8e4940ed651e014b8a067e1d6fcdbe108ab
Nullify <46113492+NullifyDev@users.noreply.github.com>
2023-06-15 12:20:55 +0100

    Update help.js
    Added Help Contents

---
+++ src/help.js
@@ -0,0 +1,9 @@
+let help = {
+  "--get-error": "\tGrabs error list when either provided with 'list' or no arguments/values. Otherwise, if argument is an error code, It will return with the provided code and its description.",
+  "--check": "\tChecks if there are any unlisted characters. If it finds even one unlisted character, it returns true",
+  "--filter": "\tfilters and pushes the characters that were found in the input only when that said character is listed in the config.",
+  "--config": "allows the user to configure the software.",
+  "\twhitelist": "allows the user to define. list, clear, remove (etc) their own allowed characters.",
+  "\t\tadd": "adds characters to the whitelist.",
+  "\t\tremove": "removes characters from the whitelist"
+}
